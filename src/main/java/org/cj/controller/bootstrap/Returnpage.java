360bca579fe1a12563f540ea0ca5a6819fd5246b
yunzhongfan <763748729@qq.com>
2017-04-21 14:25:18 +0800

    集成了spring、springMVC、mybatis框架

---
+++ src/main/java/org/cj/controller/bootstrap/Returnpage.java
@@ -0,0 +1,12 @@
+package org.cj.controller.bootstrap;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Returnpage {
+	static{
+		
+		System.out.println("21123123213");
+	}
+
+}
